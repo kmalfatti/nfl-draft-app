23673bb93a8cb2756514457691432f68238d33f3
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-08-18 15:40:10 -0700

    added alert on window close

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-for (i=0; i<data.players.length; i++){
-  console.log(i+1+':'+data.players[i])
-var yo = $('<td>').append((i+1)+': '+data.players[i].name)
-$('body').append('<hr>')
-$('body').append(yo)
-}
+data.players.forEach((cur, i)=>{
+  // console.log(i)
+  var overall = $('<td>').append(i+1)
+  var name = $('<a>').attr('href', cur.link).text(cur.name).attr('target','_blank')
+  var position = $('<td>').append(cur.position)
+  var team = $('<td>').append(cur.team)
+  var rank = $('<td>').append(cur.rank)
+  var projected = $('<td>').append(cur.projected_points)
+  var bye = $('<td>').append(cur.bye)
+
+
+  var row = $('<tr>').append(overall, name, position, team, rank, projected, bye)
+  $('tbody').append(row)
+})
+
+
+//Confirm before closing window
+window.onbeforeunload = (e)=> {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+
+    // For Safari
+    return 'Sure?';
+};
