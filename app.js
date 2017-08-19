3f8d83cf2982f493d4285e75d32eadcd06eacb32
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-08-19 10:39:59 -0700

    added undo functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+var prev;
+var timer;
+
 data.players.forEach((cur, i)=>{
-  // console.log(i)
   var overall = $('<td>').append(i+1)
   var name = $('<td>').append($('<a>').attr('href', cur.link).text(cur.name).attr('target','_blank'))
   var position = $('<td>').append(cur.position)
@@ -7,12 +9,39 @@ data.players.forEach((cur, i)=>{
   var rank = $('<td>').append(cur.rank)
   var projected = $('<td>').append(cur.projected_points)
   var bye = $('<td>').append(cur.bye)
-  var button = $('<button>').append().addClass('btn btn-danger').text('Remove')
+  var button = $('<button>').append().addClass('btn-xs btn-danger').text('Remove').on('click', remove)
 
   var row = $('<tr>').append(overall, name, position, team, rank, projected, bye, button)
   $('tbody').append(row)
 })
 
+
+function remove(){
+  prev = $(this).parent();
+  console.log(prev)
+  var undo = $('<tr>').append($('<td>').append($('<button>').text('Undo')).addClass('undo').attr('colspan', 8).append($('<span>').text(' 5')))
+  $(undo).on('click', redisplay)
+  $(this).parent().before(undo)
+  $(this).parent().hide()
+  countdown(4);
+}
+
+function countdown(i) {
+    timer = setInterval(function () {
+        $('span').text(" "+i)
+        if (i===0){
+          $('.undo').parent().hide()
+          clearInterval(timer)
+        }
+        i--;
+    }, 1000);
+}
+
+function redisplay(){
+  $(prev).show()
+  $(this).hide()
+  clearInterval(timer)
+}
 
 //Confirm before closing window
 /*
